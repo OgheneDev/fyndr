89bf1d950db0a1f8769a69407dbaa36064524be5
OgheneDev <emmanueloghene72@gmail.com>
2025-06-11 23:06:52 +0100

    Hero section

--- src/components/landing page/HeroSection.jsx
+++ src/components/landing page/HeroSection.jsx
@@ -3,13 +3,19 @@ import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <section className='flex flex-col gap-5 p-5'>
-        <div className="text-content">
+    <section className='flex flex-col md:flex-row md:gap-0 md:justify-between gap-5 p-5 md:py-10 md:px-12'>
+        <div className="text-content md:w-[40%]">
             <article className='space-y-5 mb-7'>
-                <h1 className='text-3xl font-bold'>
-                    Connect with nearby Merchants offering services you need 
+                <h1 className='text-3xl md:text-5xl font-bold'>
+                    Connect with nearby<br className='hidden md:block' /> 
+                    Merchants offering<br className='hidden md:block' />
+                     services you need 
                 </h1>
-                <p className='text-sm'>Connect with nearby Merchants offering services you need Connect with nearby Merchants offering services you need Connect with nearby Merchants offering services you need Connect with nearby Merchants offering services you need </p>
+                <p className='text-sm'>Connect with nearby Merchants offering services you<br className='hidden md:block' />
+                 need Connect with nearby Merchants offering<br className='hidden md:block' />
+                  services you need Connect with nearby Merchants<br className='hidden md:block' />
+                   offering services you need Connect with nearby<br className='hidden md:block' />
+                    Merchants offering services you need </p>
             </article>
             <div className='flex items-center gap-7'>
                 <button className='bg-[#57132A] text-white py-3 px-5 rounded-md text-sm'>
@@ -21,13 +27,13 @@ const HeroSection = () => {
             </div>
         </div>
 
-        <div className="image-content">
+        <div className="image-content md:w-[60%]">
             <Image
               src={'/images/hero.jpg'}
               width={100}
               height={100}
               alt='Hero'
-              className='w-full'
+              className='w-full h-auto'
             />
         </div>
     </section>
