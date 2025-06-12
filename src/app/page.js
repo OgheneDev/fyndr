72174ae760535d7c92983c03aec9de254c0ed302
OgheneDev <emmanueloghene72@gmail.com>
2025-06-12 09:41:17 +0100

    How it works Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/landing page/Navbar";
 import HeroSection from "@/components/landing page/HeroSection";
+import HowWeWork from "@/components/landing page/HowWeWork";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <HowWeWork />
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const howItWorks = [
+        {
+            image: '/images/picking.png',
+            title: 'Select a Service',
+            description: 'Choose from Property, Car Hire, Cleaning, or Car Parts based on what you need.'
+        },
+        {
+            image: 'images/post.png',
+            title: 'Post Your Request',
+            description: 'Fill a quick form with details like your location, what you’re looking for, and your target location or axis.'
+        },
+        {
+            image: 'images/pay.png',
+            title: 'Make a Small Payment',
+            description: 'To ensure quality and commitment, a small payment is required before merchants can see your request.'
+        },
+        {
+            images: 'images/notify.png',
+            title: 'Get Offers',
+            description: 'Verified merchants in your target area are instantly notified via WhatsApp to view your request.'
+        },
+        {
+            image: 'images/choose.png',
+            title: 'Chat & Choose',
+            description: 'Merchants who can help will reply to your request on the platform. Chat securely and choose the one that fits you best.'
+        }
+]
