a90cb3802452e9e0ccccf42b5e6807192cd7466f
OgheneDev <emmanueloghene72@gmail.com>
2025-06-14 20:01:24 +0100

    shoot

--- src/components/landing page/Footer.jsx
+++ src/components/landing page/Footer.jsx
@@ -75,14 +75,18 @@ const Footer = () => {
         {/* Newsletter subscription */}
         <div className="w-full lg:max-w-md mt-8 lg:mt-0">
           <h3 className="text-white text-xl  font-bold mb-6">Subscribe to our Newsletter</h3>
-          <div className='bg-white px-5 py-3 w-fit flex gap-0 md:gap-10 rounded-sm'>
-            <input type="email" placeholder='Your Email Address' className='placeholder:text-[12px] outline-0 placeholder:text-[#000000]' />
-            <button
-              className='bg-[#57132A] text-white text-sm py-2 px-5 rounded-sm'
-            >
-                Send
-            </button>
-        </div>
+           <div className='bg-white px-3 md:px-5 py-3 w-full md:w-fit flex flex-wrap md:flex-nowrap items-center gap-2 md:gap-10 rounded-sm'>
+              <input 
+                  type="email" 
+                  placeholder='Your Email Address' 
+                  className='w-full md:w-auto min-w-0 placeholder:text-[12px] outline-0 placeholder:text-[#000000]'
+              />
+              <button
+                  className='w-full md:w-auto bg-[#57132A] text-white text-sm py-2 px-5 rounded-sm'
+              >
+                  Send
+              </button>
+          </div>
         </div>
       </div>
     </footer>
