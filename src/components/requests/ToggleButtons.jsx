28ec50cebdc091e823212955dd8a9f75a571281f
OgheneDev <emmanueloghene72@gmail.com>
2025-07-07 13:46:59 +0100

    hm

--- src/components/requests/ToggleButtons.jsx
+++ src/components/requests/ToggleButtons.jsx
@@ -4,19 +4,27 @@ export function ToggleButtons({ activeTab, setActiveTab }) {
       <div className="relative flex w-full">
         <button
           onClick={() => setActiveTab('completed')}
-          className={`relative flex-1 py-3  text-sm font-medium transition-colors duration-300 ${
-            activeTab === 'completed' ? 'text-[#121417] border-b-2 border-[#57132A]' : 'text-gray-400'
+          className={`relative flex-1 py-3 text-sm font-medium transition-colors duration-300 ${
+            activeTab === 'completed' ? 'text-[#121417]' : 'text-gray-400'
           }`}
         >
           Live Requests
+          {activeTab === 'completed' && (
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-1 bg-[#57132A] rounded-t-md" 
+                 style={{ width: 'calc(100% - 2rem)' }}></div>
+          )}
         </button>
         <button
           onClick={() => setActiveTab('pending')}
-          className={`relative flex-1 py-3  text-sm font-medium transition-colors duration-300 ${
-            activeTab === 'pending' ? 'text-[#121417] border-b-2 border-[#57132A]' : 'text-gray-400'
+          className={`relative flex-1 py-3 text-sm font-medium transition-colors duration-300 ${
+            activeTab === 'pending' ? 'text-[#121417]' : 'text-gray-400'
           }`}
         >
           Awaiting Payment
+          {activeTab === 'pending' && (
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-1 bg-[#57132A] rounded-t-md" 
+                 style={{ width: 'calc(100% - 2rem)' }}></div>
+          )}
         </button>
       </div>
     </div>
