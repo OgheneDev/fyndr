75ca07994ab13f487743fd0dc0c4bb011afcddfa
OgheneDev <emmanueloghene72@gmail.com>
2025-07-11 10:38:25 +0100

    sha

--- src/components/request/InterestedMerchants.jsx
+++ src/components/request/InterestedMerchants.jsx
@@ -5,18 +5,18 @@ const InterestedMerchants = ({ merchants, onAccept, actionLoading }) => (
     <div className="space-y-4">
       {merchants.map((merchant) => (
         <div key={merchant._id} className="bg-gray-100 shadow-md p-4 rounded-xl">
-          <p className="mb-2">
+          <p className="mb-2 text-sm">
             <span className="">Name:</span>{" "}
             {merchant.merchant?.name ||
               merchant.merchant?.businessName ||
               merchant.merchant?.email ||
               "Unknown"}
           </p>
-          <p className="mb-2 text-gray-500">
+          <p className="mb-2 text-gray-500 text-sm">
             <span className="">Message:</span>{" "}
             {merchant.message?.content || merchant.message || ""}
           </p>
-          <p className="mb-2">
+          <p className="mb-2 text-sm">
             <span className="">Status:</span>{" "}
             {merchant.isAccepted ? (
               <span className="text-green-700 font-semibold">Accepted</span>
@@ -26,7 +26,7 @@ const InterestedMerchants = ({ merchants, onAccept, actionLoading }) => (
           </p>
           {!merchant.isAccepted && (
             <button
-              className="border border-gray-300 hover:border-gray-400 hover:bg-gray-50 text-gray-700 px-4 py-2 rounded-md mt-2 disabled:opacity-60 disabled:cursor-not-allowed transition-all duration-200"
+              className="border border-gray-300 text-sm hover:border-gray-400 hover:bg-gray-50 text-gray-700 px-4 py-2 rounded-md mt-2 disabled:opacity-60 disabled:cursor-not-allowed transition-all duration-200"
               onClick={() => onAccept(merchant._id)}
               disabled={actionLoading}
             >
